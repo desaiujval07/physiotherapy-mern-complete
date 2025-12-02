7740fc8d11966744f905649c1d0899f610083d0b
desaiujval07 <ujvaldesai07@gmail.com>
2025-12-02 14:17:20 +0530

    Add appointment routes

---
+++ backend/routes/appointmentRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {
+  getAppointments,
+  getAppointment,
+  createAppointment,
+  updateAppointment,
+  deleteAppointment
+} = require('../controllers/appointmentController');
+const { protect } = require('../middleware/auth');
+
+const router = express.Router();
+
+router.route('/')
+  .get(protect, getAppointments)
+  .post(protect, createAppointment);
+
+router.route('/:id')
+  .get(protect, getAppointment)
+  .put(protect, updateAppointment)
+  .delete(protect, deleteAppointment);
+
+module.exports = router;
